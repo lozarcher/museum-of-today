f7d9f9d13333cbadf210b67688281da657baf089
Loz <loz@the-pub.org>
2020-10-13 23:26:15 +0100

    Attempted (and aborted) mouselook

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -7,6 +7,14 @@ public class CameraController : MonoBehaviour
     public float transitionSpeed;
     Transform currentView;
 
+    //Unused, for mouselook
+    /* 
+    bool currentlyMoving = false;
+    const float positionsPrettyClose = 0.1f;
+    private Vector3 lastMouse = new Vector3(Screen.height / 2, Screen.width / 2, 0); //kind of in the middle of the screen, rather than at the top (play)
+    float camSens = 1f; //How sensitive it with mouse
+    */
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,48 +23,55 @@ public class CameraController : MonoBehaviour
     public void setView(Transform newView)
     {
         currentView = newView;
-        //Debug.Log("New View " + currentView.position.x + " " + currentView.position.y + " " + currentView.position.z);
-        //Debug.Log("Transition speed " + transitionSpeed);
-
-        //Debug.Log("Set new view in camera controller");
+        //currentlyMoving = true;
     }
 
     void LateUpdate()
     {
         if (currentView != null)
         {
-
-            //Debug.Log("Target View " + currentView.position.x + " " + currentView.position.y + " " + currentView.position.z);
-
-            //Debug.Log("Transform "+transform.position.x + " " + transform.position.y + " " + transform.position.z);
-
-        //Lerp position
-        transform.position = Vector3.Lerp(transform.position, currentView.position, Time.deltaTime * transitionSpeed);
-            Vector3 currentAngle = new Vector3(
-                Mathf.LerpAngle(transform.rotation.eulerAngles.x, currentView.rotation.eulerAngles.x, Time.deltaTime * transitionSpeed),
-                Mathf.LerpAngle(transform.rotation.eulerAngles.y, currentView.rotation.eulerAngles.y, Time.deltaTime * transitionSpeed),
-                Mathf.LerpAngle(transform.rotation.eulerAngles.z, currentView.rotation.eulerAngles.z, Time.deltaTime * transitionSpeed)
-            );
-            transform.eulerAngles = currentAngle;
+                transform.position = Vector3.Lerp(transform.position, currentView.position, Time.deltaTime * transitionSpeed);
+                Vector3 currentAngle = new Vector3(
+                    Mathf.LerpAngle(transform.rotation.eulerAngles.x, currentView.rotation.eulerAngles.x, Time.deltaTime * transitionSpeed),
+                    Mathf.LerpAngle(transform.rotation.eulerAngles.y, currentView.rotation.eulerAngles.y, Time.deltaTime * transitionSpeed),
+                    Mathf.LerpAngle(transform.rotation.eulerAngles.z, currentView.rotation.eulerAngles.z, Time.deltaTime * transitionSpeed)
+                );
+                transform.eulerAngles = currentAngle;
         }
-
     }
+    /* unused, for mouselook
+    void LateUpdate() {
+        if (currentView != null) {
+            if (currentlyMoving) {
+                if (Vector3.Distance(transform.position, currentView.position) < positionsPrettyClose)
+                {
+                    currentlyMoving = false;
+                }
+            }
 
-    // alternative way of detecting mouse clicks on objects, globally
-    // private void Update()
-    //{
-    //    if (Input.GetMouseButtonDown(0))
-    //    { // if left button pressed...
-    //        Ray ray = GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
-    //        RaycastHit hit;
-    //        if (Physics.Raycast(ray, out hit))
-    //        {
-    //            Debug.Log("Detected HIT on "+hit.collider.gameObject);
-    //            //Debug.Log("Detected hit : " + hit.collider.gameObject.name.ToString);
-    //            // the object identified by hit.transform was clicked
-    //            // do whatever you want
-    //        }
-    //    }
-    //}
+            if (currentlyMoving)
+            {
+                //Lerp position to new target
+                transform.position = Vector3.Lerp(transform.position, currentView.position, Time.deltaTime * transitionSpeed);
+                Vector3 currentAngle = new Vector3(
+                    Mathf.LerpAngle(transform.rotation.eulerAngles.x, currentView.rotation.eulerAngles.x, Time.deltaTime * transitionSpeed),
+                    Mathf.LerpAngle(transform.rotation.eulerAngles.y, currentView.rotation.eulerAngles.y, Time.deltaTime * transitionSpeed),
+                    Mathf.LerpAngle(transform.rotation.eulerAngles.z, currentView.rotation.eulerAngles.z, Time.deltaTime * transitionSpeed)
+                );
+                transform.eulerAngles = currentAngle;
+            }
+            else
+            {
+                // Allow Mouse Movement
+                Debug.Log("mouse position " + Input.mousePosition);
+                lastMouse = Input.mousePosition - lastMouse;
+                lastMouse = new Vector3(-lastMouse.y * camSens, lastMouse.x * camSens, 0);
+                lastMouse = new Vector3(transform.eulerAngles.x + lastMouse.x, transform.eulerAngles.y + lastMouse.y, 0);
+                transform.eulerAngles = lastMouse;
+                lastMouse = Input.mousePosition;
+            }
+        }
+    }
+    */
 
 }
